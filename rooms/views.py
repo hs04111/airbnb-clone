f5f2dbd824a3cff23e113870e5b7290321d905dc
hs04111 <hs04111@naver.com>
2020-12-30 23:44:05 +0900

    RoomDetail view class added in rooms.views

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,5 @@
-from django.views.generic import ListView
-from django.http import Http404
+from django.views.generic import ListView, DetailView
 
-# from django.urls import reverse
-from django.shortcuts import render  # , redirect
 from . import models
 
 
@@ -17,12 +14,23 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
-        # return redirect(reverse("core:home"))
-        # 방 pk가 모델에 없는 경우 redirect로 home으로 보낼 수도 있고, 아니면 404 에러를 일으킬 수 있다.
-        # Http404를 일으키는 경우, 만약 templates 폴더에 404.html이 있으면 debug=True일 경우 이 html이 render된다.
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+
+# from django.http import Http404
+# from django.urls import reverse
+# from django.shortcuts import render  , redirect
+
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
+#         # return redirect(reverse("core:home"))
+#         # 방 pk가 모델에 없는 경우 redirect로 home으로 보낼 수도 있고, 아니면 404 에러를 일으킬 수 있다.
+#         # Http404를 일으키는 경우, 만약 templates 폴더에 404.html이 있으면 debug=True일 경우 이 html이 render된다.
